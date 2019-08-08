49cd41c1aeb4d7236892b99beec57ada39ac5b34
Weston Harper <wes@tao.team>
2019-08-07 20:27:30 -0600

    feat: add deploy script and changed README

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+npm run build
+
+cd dist
+
+echo 'snuze.app' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:wesharper/snuze.app.git master:gh-pages
+cd -
